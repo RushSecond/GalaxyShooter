ad00ec140a7fbee759c8aa32d43c9e51e9eef23d
Blake Z <blake.rushhour@gmail.com>
2021-04-24 15:07:59 -0700

    Player input and screen bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,11 +17,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0f);
+
         // Move the player by "mySpeed" units, every second
-        transform.position += Vector3.right * horizontalInput * _mySpeed * Time.deltaTime;
-        transform.position += Vector3.up * verticalInput * _mySpeed * Time.deltaTime;
+        transform.position += direction * _mySpeed * Time.deltaTime;
+
+        // if player position y is greater than 0
+        // set position y to 0
+        // if player position y < -4.5
+        // set position y to 4.5
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.5f, 0);
+        }
+
+        // screen wrap on x.
+        // if player position x is outside screen bounds -> abs(x) > 9.4f
+        // set position x to opposite side of screen -> -9.4f * the sign of the position
+        float xPosition = transform.position.x;
+        if (Mathf.Abs(xPosition) > 9.4f)
+        {
+            transform.position = new Vector3(-9.4f * Mathf.Sign(xPosition), transform.position.y, 0);
+        }
     }
 }
