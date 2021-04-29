bc75f6ccc3f534cacb08a89d2fc08f5f0ac91736
Blake Z <blake.rushhour@gmail.com>
2021-04-28 19:38:10 -0700

    Small changes to spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,30 +9,18 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _spawnEnemy;
 
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // spawn game objects every 5 seconds
-    // create a coroutine of type IEnumerator
-    // with a while loop
-
     IEnumerator SpawnRoutine()
-    {
+    { 
+        WaitForSeconds waitSeconds = new WaitForSeconds(_spawnTime);
+
         // while loop (infinite loop)
         // Instantiate enemy prefab
         // yield wait for 5 seconds
-
-        WaitForSeconds waitSeconds = new WaitForSeconds(_spawnTime);
-
         while (true)
         {
             Instantiate(_spawnEnemy, Enemy.RandomPositionAtTop(), Quaternion.identity);
@@ -40,3 +28,4 @@ public class SpawnManager : MonoBehaviour
         }
     }
 }
+
