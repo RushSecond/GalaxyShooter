31ee97e178e4a7788510a1e980d8d17a9b10d044
Blake Z <blake.rushhour@gmail.com>
2021-05-08 21:12:43 -0700

    Enemies now explode when they are destroyed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,12 +11,19 @@ public class Enemy : MonoBehaviour
 
     private UIManager _UIManager;
 
+    // Animator component
+    private Animator _myAnimator;
+
     void Start()
     {
-        // Setup the UI manager
         _UIManager = GameObject.Find("Canvas").GetComponent<UIManager>();
         if (!_UIManager)
             Debug.LogError(this + " an enemy couldn't find the UIManager script.");
+        
+        // Setup and null check
+        _myAnimator = GetComponent<Animator>();
+        if (!_myAnimator)
+            Debug.LogError(this + " an enemy doesn't have an animator.");
     }
 
     void Update()
@@ -38,16 +45,28 @@ public class Enemy : MonoBehaviour
             Player playerScript = other.GetComponent<Player>();
             if (playerScript)
                 playerScript.Damage();
-            Destroy(gameObject);
+            EnemyDeath();
         }
 
         if (other.tag == "Laser")
         {
-            // Tell the UIManager to add 10 to the score
             _UIManager.AddScore(_scoreValue);
 
             Destroy(other.gameObject);
-            Destroy(gameObject);
+            EnemyDeath();
         }
+    }
+
+    private void EnemyDeath()
+    {
+        // Enemy should no longer move or collide
+        _mySpeed = 0f;
+        GetComponent<Collider2D>().enabled = false;
+
+        // Play animation
+        _myAnimator.SetTrigger("OnEnemyDeath");
+
+        // Destroy after 5 seconds so the animation can play
+        Destroy(gameObject, 5f);
     }
 }
